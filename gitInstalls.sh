210d70480b238d59d6f5702ffe3ee54a1028c5b1
donnydcy <donnydcy@users.noreply.github.com>
2017-05-05 13:10:22 -0400

    inital git packages

---
+++ gitInstalls.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cd ~/
+mkdir gitPackages
+cd gitPackages
+# Install singularity
+VERSION=2.2.1
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+sudo make install
+
